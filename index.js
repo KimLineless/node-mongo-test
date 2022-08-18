1eb97265d4202bcd3b8bdff9b604e0358437ac84
Lineless <2837599@naver.com>
2022-08-18 14:12:10 +0900

    test

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+let db;
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+MongoClient.connect(
+  'mongodb+srv://asd3qwe1:antjs15915!@cluster0.sfj9t0l.mongodb.net/?retryWrites=true&w=majority',
+  function (testerr, client) {
+    if (testerr) return console.log(testerr);
+    db = client.db('todolist');
+    db.collection('lists').insertOne({ 이름: '김무선', _id: 2 }, function (
+      testerr,
+      response
+    ) {
+      console.log('저장완료');
+    });
+    app.listen('8080', function () {
+      console.log('정상작동');
+    });
+  }
+);
+
+app.get('/', function (request, response) {
+  response.sendFile(__dirname + '/index.html');
+});
+
+app.get('/sub1', function (request, response) {
+  response.sendFile(__dirname + '/sub1.html');
+});
+
+app.get('/sub2', function (request, response) {
+  response.sendFile(__dirname + '/sub2.html');
+});
+
+app.post('/add', function (request, response) {
+  console.log(request.body);
+  db.collection('post').insertOne(
+    {
+      제목: request.body.title,
+      날짜: request.body.date,
+    },
+    function () {
+      response.redirect('/list');
+    }
+  );
+});
+
+app.get('/list', function (request, response) {
+  db.collection('post')
+    .find()
+    .toArray(function (testerr, result) {
+      console.log(result);
+      response.render('list.ejs', { post: result });
+    });
+});
